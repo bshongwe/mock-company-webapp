16328795a94ded8f3c4083d75271dd118b5a4327
bshongwe <dru.da.beat.hustle@gmail.com>
2024-04-16 18:55:57 +0200

    Jenkins: continuous intergration build fail guard addition

--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -28,47 +28,53 @@ public class SearchService {
          *   2. query string is treated as case-insensitive meaning Hi will match hi, hI, Hi, or HI
          *   3. If the query is wrapped in quotes, only EXACT matches of name/description will be returned
          */
-        Iterable<ProductItem> allItems = this.productItemRepository.findAll();
-        List<ProductItem> itemList = new ArrayList<>();
+        //Iterable<ProductItem> allItems = this.productItemRepository.findAll();
+        //List<ProductItem> itemList = new ArrayList<>();
 
         /*
          * 1. Check for quotes in the query string. We should use a regex for this but for simplicity
          *    we will just check and extract using startsWith/endsWith/subString
          */
-        boolean exactMatch = false;
-        if (query.startsWith("\"") && query.endsWith("\"")) {
+        //boolean exactMatch = false;
+        //if (query.startsWith("\"") && query.endsWith("\"")) {
             exactMatch = true;
             // Extract the quotes
             query = query.substring(1, query.length() - 1);
-        } else {
+        //} else {
             // Handle case-insensitivity by converting to lowercase first
-            query = query.toLowerCase();
-        }
+        //    query = query.toLowerCase();
+        //}
 
         // For each item... This is written for simplicity to be read/understood not necessarily maintain or extend
-        for (ProductItem item : allItems) {
-            boolean nameMatches;
-            boolean descMatches;
+        //for (ProductItem item : allItems) {
+        //    boolean nameMatches;
+        //    boolean descMatches;
             // Check if we are doing exact match or not
-            if (exactMatch) {
+        //    if (exactMatch) {
                 // Check if name is an exact match
-                nameMatches = query.equals(item.getName());
+        //        nameMatches = query.equals(item.getName());
                 // Check if description is an exact match
-                descMatches = query.equals(item.getDescription());
-            } else {
+        //        descMatches = query.equals(item.getDescription());
+        //    } else {
                 // We are doing a contains ignoring case check, normalize everything to lowercase
                 // Check if name contains query
-                nameMatches = item.getName().toLowerCase().contains(query);
+        //        nameMatches = item.getName().toLowerCase().contains(query);
                 // Check if description contains query
-                descMatches = item.getDescription().toLowerCase().contains(query);
-            }
+        //        descMatches = item.getDescription().toLowerCase().contains(query);
+        //    }
 
             // If either one matches, add to our list
-            if (nameMatches || descMatches) {
-                itemList.add(item);
-            }
-        }
+        //    if (nameMatches || descMatches) {
+        //        itemList.add(item);
+        //    }
+        //}
         // Return results
-        return itemList;
+	/* SOLUTION: by commenting out the code, an empty list is created
+	 * also replaced itemList with Collections.emptyList() so as to
+	 * return immutable empty list to ensure empty list return for
+	 * every input query
+	 */
+        //return itemList;
+	return Collections.emptyList();
     }
 }
